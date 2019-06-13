a8c71f4ff42ccd8e52f18fca435c644df825b629
Oleg Arbuzov <boozze@mail.ru>
2019-06-13 11:43:50 +0300

    исправлены названия массивов

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ var WINDOW_HEIGHT_MAX = 630;
 var WINDOW_HEIGHT_MIN = 130;
 
 var accomodationType = ['palace', 'flat', 'house', 'bungalo'];
-var announcement = [];
+var announcements = [];
 
 var getRandom = function (min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
@@ -18,7 +18,7 @@ var getAccomodation = function (types) {
   return types[getRandom(0, types.length - 1)];
 };
 
-var arrayFill = function (array, quantity) {
+var arrayFill = function (objects, quantity) {
   for (var i = 1; i <= quantity; i++) {
     var avatarSource = i < 10 ? 'img/avatars/user0' + i + '.png' : 'img/avatars/user' + i + '.png';
     var tempObject = {
@@ -38,13 +38,13 @@ var arrayFill = function (array, quantity) {
       }
 
     };
-    array.push(tempObject);
+    objects.push(tempObject);
   }
 };
 
 var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
 
-arrayFill(announcement, 8);
+arrayFill(announcements, 8);
 
 var renderPin = function (pin) {
   var element = pinTemplate.cloneNode(true);
@@ -58,8 +58,8 @@ var renderPin = function (pin) {
 };
 
 var fragment = document.createDocumentFragment();
-for (var i = 0; i < announcement.length; i++) {
-  fragment.appendChild(renderPin(announcement[i]));
+for (var i = 0; i < announcements.length; i++) {
+  fragment.appendChild(renderPin(announcements[i]));
 }
 
 document.querySelector('.map__pins').appendChild(fragment);
