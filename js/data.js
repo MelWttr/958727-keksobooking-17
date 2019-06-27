76237ae3a7a78d63349b2def02881eb4fddd3981
Oleg Arbuzov <boozze@mail.ru>
2019-06-27 22:12:32 +0300

    разбил на модули

---
+++ js/data.js
@@ -0,0 +1,40 @@
+'use strict';
+(function () {
+  var map = document.querySelector('.map');
+
+  var WINDOW_WIDTH = map.offsetWidth;
+  var WINDOW_HEIGHT_MAX = 630;
+  var WINDOW_HEIGHT_MIN = 130;
+
+  // парсит в число из строки координату абсолютно спозиционированного элемента
+  var extractCoord = function (str) {
+    return parseInt(str.replace('px', ''), 10);
+  };
+
+  // возвращает координату X элемента (пина)
+  var getX = function (element, width) {
+    return extractCoord(element.style.left) + width;
+  };
+
+  // возвращает координату Y элемента (пина)
+  var getY = function (element, height) {
+    return extractCoord(element.style.top) + height;
+  };
+
+  var getRandom = function (min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  };
+
+
+  window.data = {
+    map: map,
+    WINDOW_WIDTH: WINDOW_WIDTH,
+    WINDOW_HEIGHT_MIN: WINDOW_HEIGHT_MIN,
+    WINDOW_HEIGHT_MAX: WINDOW_HEIGHT_MAX,
+    getX: getX,
+    getY: getY,
+    extractCoord: extractCoord,
+    getRandom: getRandom
+  };
+
+})();
---
+++ js/form-validation.js
@@ -0,0 +1,74 @@
+'use strict';
+(function () {
+  // хранит минимальные стоимости за ночь для разных типов жилья
+  var MIN_PRICES = {
+    'bungalo': '0',
+    'flat': '1000',
+    'house': '5000',
+    'palace': '10000'
+  };
+  var form = document.querySelector('.ad-form');
+  var address = form.querySelector('#address');
+  var type = document.querySelector('#type');
+  var price = document.querySelector('#price');
+
+  // переключает поле из активного в неактивное состояние и наоборот
+  var toggleElement = function (field, isDisabled) {
+    field.disabled = isDisabled;
+  };
+
+  var field = form.querySelector('.ad-form-header__input');
+  var fieldSets = form.querySelectorAll('.ad-form__element');
+
+  //  переключает все поля формы на странице из активного в неактивное состояние и наоборот
+  var toggleFields = function (isDisabled) {
+    toggleElement(field, isDisabled);
+    for (var i = 0; i < fieldSets.length; i++) {
+      toggleElement(fieldSets[i], isDisabled);
+    }
+  };
+
+  toggleFields(true); // выключает все поля в форме
+
+  var mainPin = window.data.map.querySelector('.map__pin--main');
+
+  // возвращает строку с координатами x и y для записи в поле адрес
+  var makeAddressValue = function (x, y) {
+    return x + ',' + y;
+  };
+
+  window.formValidation = {
+    mainPin: mainPin,
+    address: address,
+    form: form,
+    toggleFields: toggleFields,
+    makeAddressValue: makeAddressValue
+  };
+
+  // заполняет поле с адресом координатами главного пина
+  address.value = makeAddressValue(window.data.getX(mainPin, mainPin.offsetWidth / 2), window.data.getY(mainPin, mainPin.offsetHeight / 2));
+
+  // устанавливаем первоначальные значения полей тип и цена за ночь
+  price.min = MIN_PRICES[type.value];
+  price.placeholder = MIN_PRICES[type.value];
+
+  // синхронизируем поля тип и цена за ночь
+  type.onchange = function () {
+    price.min = MIN_PRICES[this.value];
+    price.placeholder = MIN_PRICES[this.value];
+  };
+
+  // устанавливаем первоначальные значения полей дат заезда и выезда
+  form.timein.value = form.timeout.value;
+
+  // синхронизируем дату заезда и выезда
+  form.onchange = function (evt) {
+    if (evt.target.name === 'timein') {
+      this.timeout.value = evt.target.value;
+    }
+    if (evt.target.name === 'timeout') {
+      this.timein.value = evt.target.value;
+    }
+  };
+})();
+
